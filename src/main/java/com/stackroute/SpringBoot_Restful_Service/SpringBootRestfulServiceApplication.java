cd6a5d82fcb8a585ef5c3d81cd60cd8d62563050
Arathi <mj.arathi@gmail.com>
2019-05-21 14:54:14 +0530

    initial commit

---
+++ src/main/java/com/stackroute/SpringBoot_Restful_Service/SpringBootRestfulServiceApplication.java
@@ -0,0 +1,13 @@
+package com.stackroute.SpringBoot_Restful_Service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulServiceApplication.class, args);
+	}
+
+}
